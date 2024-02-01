8e46477e3ef71b909563fd032fdd11a5c769a184
Nanagabby95 <nancitonana86@gmail.com>
2024-02-01 10:32:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+    function myButton() {
+                document.getElementById("demo").innerHTML="What's Up, You called Me"
+            }
+            function mySecondButton() {
+                document.getElementById("demo2").innerHTML="No Please!"
+            }
+            function whyButton() {
+                document.getElementById("demo3").innerHTML="Sorry, I'm Working"
+            }
+
+            function chatLater(){
+                document.getElementById("demo4").innerHTML="What Time?"
+            }
+            
+            function noon(){
+               
+                document.getElementById( "demo5").innerHTML="Yes"
+            }
+            function cool(){
+                document.getElementById("demo6").innerHTML="Bye"
+            }
+            function sure(){
+                document.getElementById("demo7").innerHTML="thanks"
+            }
+
+            function can(){
+            document.getElementById("demo8").innerHTML="same here"
+            }
+            function time(){
+                document.getElementById("demo9").innerHTML=Date()
+            }
